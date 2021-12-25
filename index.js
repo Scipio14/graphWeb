2498b794a86bee25e30dbcbaf195b4c7890172e7
Federico Macías <fma.abogado@gmail.com>
2021-12-25 09:46:31 -0600

    Se crea el servidor de graphql y se crea el schema con la RootQueryType y el campo de búsqueda de los libros

---
+++ index.js
@@ -0,0 +1,69 @@
+import express from "express";
+const app = express();
+import expressGraphQL, { graphqlHTTP } from "express-graphql";
+import {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLID,
+  GraphQLList,
+  GraphQLNonNull,
+  GraphQLInt,
+} from "graphql";
+const PORT = 1414;
+
+const authors = [
+  { id: 1, name: "J.K.Rowling" },
+  { id: 2, name: "Jane Austen" },
+  { id: 3, name: "C.J. Sansom" },
+  { id: 4, name: "J.R.R. Tolkien" },
+];
+
+const books = [
+  { id: 1, name: "Harry Potter and the Chamber of Secrets", authorId: 1 },
+  { id: 2, name: "Harry Potter and the Prisoner of Azkaban", authorId: 1 },
+  { id: 3, name: "Pride and Prejudice", authorId: 2 },
+  { id: 4, name: "Dissolution", authorId: 3 },
+  { id: 5, name: "Dark Fire", authorId: 3 },
+  { id: 6, name: "Sovereign", authorId: 3 },
+  { id: 7, name: "The Fellowship of the Ring", authorId: 4 },
+  { id: 8, name: "The Two Towers", authorId: 4 },
+];
+
+const BookType = new GraphQLObjectType({
+  name: "Books",
+  description: "This represents a book written by an author",
+  fields: () => ({
+    id: { type: new GraphQLNonNull(GraphQLID) },
+    name: { type: new GraphQLNonNull(GraphQLString) },
+    authorId: { type: new GraphQLNonNull(GraphQLInt) },
+  }),
+});
+
+const RootQueryType = new GraphQLObjectType({
+  name: "Query",
+  description: "Root Query",
+  fields: () => ({
+    books: {
+      type: new GraphQLList(BookType),
+      description: "List of all books",
+      resolve: () => books,
+    },
+  }),
+});
+
+const schema = new GraphQLSchema({
+  query: RootQueryType,
+});
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  })
+);
+
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
+});
